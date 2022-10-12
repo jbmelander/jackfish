51015b0511b6a3f7febbba77d3edf90560ce0212
Magneto <baccuslab@magneto.stanford.edu>
2022-10-12 09:27:58 -0700

    aesthetic changes

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-pyuic5 -o /home/baccuslab/src/jackfish/gui.py /home/baccuslab/src/jackfish/gui.ui
-python3 /home/baccuslab/src/jackfish/main.py
+pyuic5 -o /home/baccuslab/src/jackfish/main_gui.py /home/baccuslab/src/jackfish/main_gui.ui
+pyuic5 -o /home/baccuslab/src/jackfish/cam_gui.py /home/baccuslab/src/jackfish/cam_gui.ui
+pyuic5 -o /home/baccuslab/src/jackfish/daq_gui.py /home/baccuslab/src/jackfish/daq_gui.ui
