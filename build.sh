102cf7fdc56bc10e13b378c188092cb4738fb5ab
Minseung Choi <minseung@stanford.edu>
2022-11-23 15:58:30 -0800

    build.sh takes in root directory arg

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
-pyuic5 -o /home/baccuslab/src/jackfish/jackfish/gui/main_gui.py /home/baccuslab/src/jackfish/jackfish/gui/main_gui.ui
-pyuic5 -o /home/baccuslab/src/jackfish/jackfish/gui/cam_gui.py /home/baccuslab/src/jackfish/jackfish/gui/cam_gui.ui
-pyuic5 -o /home/baccuslab/src/jackfish/jackfish/gui/daq_gui.py /home/baccuslab/src/jackfish/jackfish/gui/daq_gui.ui
+if [ -z "$1" ]
+then
+	echo "Must specify Jackfish root directory."
+	exit 1
+fi
+
+JACKFISH_DIR=$1
+
+pyuic5 -o $JACKFISH_DIR/jackfish/gui/main_gui.py $JACKFISH_DIR/jackfish/gui/main_gui.ui
+pyuic5 -o $JACKFISH_DIR/jackfish/gui/cam_gui.py $JACKFISH_DIR/jackfish/gui/cam_gui.ui
+pyuic5 -o $JACKFISH_DIR/jackfish/gui/daq_gui.py $JACKFISH_DIR/jackfish/gui/daq_gui.ui
